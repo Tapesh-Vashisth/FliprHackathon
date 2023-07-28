37c1769f512a8264111bd5551e6022489d2a6eb0
saipranithswargam <saipranithswargam@gmail.com>
2023-07-28 17:07:51 +0530

    added 404 page

--- client/src/pages/Error404.jsx
+++ client/src/pages/Error404.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
 
 function Error404() {
-  return (
-    <div>Error404</div>
-  )
+    return (
+        <>
+            <div className="body bg-purple">
+                <div class="stars">
+                    <div class="central-body">
+                        <img
+                            class="image-404"
+                            src="http://salehriaz.com/404Page/img/404.svg"
+                            width="300px"
+                            alt=""
+                        />
+                        <a
+                            href="http://salehriaz.com/404Page/404.html"
+                            class="btn-go-home"
+                            target="/"
+                        >
+                            GO BACK HOME
+                        </a>
+                    </div>
+                    <div class="objects">
+                        <img
+                            class="object_rocket"
+                            src="http://salehriaz.com/404Page/img/rocket.svg"
+                            width="40px"
+                            alt=""
+                        />
+                        <div class="earth-moon">
+                            <img
+                                class="object_earth"
+                                src="http://salehriaz.com/404Page/img/earth.svg"
+                                width="100px"
+                                alt=""
+                            />
+                            <img
+                                class="object_moon"
+                                src="http://salehriaz.com/404Page/img/moon.svg"
+                                width="80px"
+                                alt=""
+                            />
+                        </div>
+                        <div class="box_astronaut">
+                            <img
+                                class="object_astronaut"
+                                src="http://salehriaz.com/404Page/img/astronaut.svg"
+                                width="140px"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    <div class="glowing_stars">
+                        <div class="star"></div>
+                        <div class="star"></div>
+                        <div class="star"></div>
+                        <div class="star"></div>
+                        <div class="star"></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default Error404
+export default Error404;
