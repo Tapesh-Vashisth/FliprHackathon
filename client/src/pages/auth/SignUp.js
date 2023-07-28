2df7d750b957d3860810c5c7aef8b85f3c413924
saipranithswargam <saipranithswargam@gmail.com>
2023-07-28 20:15:09 +0530

    added signup form

--- client/src/pages/auth/SignUp.js
+++ client/src/pages/auth/SignUp.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-
 const SignUp = () => {
     return (
         <div className="signup page-signup">
-            <div className="signup__form">
-                <form>
-                    hello
+            <div className="page-signup__form">
+                <form className="page-signup__form--mainform">
+                    <h1 className="page-signup__form--heading">SignUp</h1>
+                    <div className="page-signup__form--input_group">
+                        <label className="page-signup__form--input_group--label">
+                            Name
+                        </label>
+                        <input className="page-signup__form--input_group--input" />
+                    </div>
                 </form>
             </div>
         </div>
