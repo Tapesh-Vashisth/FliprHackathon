52d2ced3d36a8745a703a9259c3d04a1828f9c49
tapesh <tapeshvashisth789@gmail.com>
2023-07-28 17:01:24 +0530

    loader_and_error_page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import PageLoader from './components/Loaders/PageLoader';
+const Error404 = React.lazy(() => import("./pages/Error404"));
 
 function App() {
   return (
     <>
       <Routes>
+
+
         
+
+        <Route 
+          path='*' 
+          element = {
+            <React.Suspense fallback = {<PageLoader />} >
+              <Error404 />
+            </React.Suspense>
+          } 
+        />
+
       </Routes>
     </>
   );
---
+++ client/src/components/Loaders/PageLoader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PageLoader() {
+  return (
+    <div>PageLoader</div>
+  )
+}
+
+export default PageLoader
