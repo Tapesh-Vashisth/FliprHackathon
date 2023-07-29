015d1a8513ad72b85f33625949902ab9fd4b1746
unknown <pranauv1803@gmail.com>
2023-07-29 17:21:25 +0530

    fixed minor bug

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,10 +6,10 @@ import axiosInstance from "./api/axiosInstance";
 import AuthProtected from "./components/AuthProtected";
 import { useAppDispatch } from "./app/hooks";
 import { userActions } from "./features/userSlice";
+import { ToastContainer, toast } from 'react-toastify'
 const Error404 = React.lazy(() => import("./pages/Error404"));
 const Login = React.lazy(() => import("./pages/auth/Login"));
 const Signup = React.lazy(() => import("./pages/auth/SignUp"));
-import { ToastContainer, toast } from 'react-toastify'
 
 function App() {
     const [screenLoad, setScreenLoad] = useState(false);
@@ -25,10 +25,10 @@ function App() {
         setScreenLoad(false);
       } catch (err: any) {
         console.log(err);
+        setScreenLoad(false);
         toast.error('Login Again! User session Expired!',{
           position: 'top-right'
         })
-        setScreenLoad(false);
       }
     }
 
