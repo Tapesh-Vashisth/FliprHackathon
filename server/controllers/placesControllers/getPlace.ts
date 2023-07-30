cf7550c6f7fff09360f4bda31a7f3aae3f4b2bd3
unknown <pranauv1803@gmail.com>
2023-07-30 14:36:18 +0530

    small change - added getPlace.ts

---
+++ server/controllers/placesControllers/getPlace.ts
@@ -0,0 +1,6 @@
+import Place from "../../models/Place"
+import axios from "axios"
+
+export const getPlace = async () => {
+    const categories = ['accomodation','tourism.attraction','tourism.sights','religion.place_of_worship','commercial.shopping_mall','commercial.gas','catering.restaurant']
+}
