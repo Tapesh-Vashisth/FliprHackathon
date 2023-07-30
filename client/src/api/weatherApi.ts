0c2f9dd7cc8f8a1e4b6a9b0595ad6662ac2842f2
tapesh <tapeshvashisth789@gmail.com>
2023-07-30 21:58:33 +0530

    sidebar

--- client/src/api/weatherApi.ts
+++ client/src/api/weatherApi.ts
@@ -2,21 +2,37 @@ import config from "../helper/config"
 import axios from "axios"
 
 const weatherApi = async (lat: Number, lon: Number) => {
+    console.log(lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${config.WEATHER_API_KEY}`
 
-    const data: any = await axios.get(url)
+    try {
+        const data: any = await axios.get(url)
+        
+        console.log(data);
+        const result = 
+        {
+            weather_desc: data.data.weather[0].description,
+            current_temp: data.data.main.temp,
+            feels_like: data.data.main.feels_like,
+            min: data.data.main.temp_min,
+            max: data.data.main.temp_max,
+            humidity: data.data.main.humidity
+        }
     
-    const result = 
-    {
-        weather_desc: data.weather.description,
-        current_temp: data.main.temp,
-        feels_like: data.main.feels_like,
-        min: data.main.temp_min,
-        max: data.main.temp_max,
-        humidity: data.main.humidity
-    }
+        return result
+    } catch (err: any) {
+        console.log(err);
+        const result = {
+            weather_desc: null, 
+            current_temp: null,
+            feels_like: null,
+            min: null,
+            max: null,
+            humidity: null
+        }
 
-    return result
+        return result;
+    }
 }
 
 export default weatherApi
--- client/src/components/PlaceSidebar.tsx
+++ client/src/components/PlaceSidebar.tsx
@@ -3,6 +3,7 @@ import CloseIcon from '@mui/icons-material/Close';
 import axiosInstance from "../api/axiosInstance";
 import config from '../helper/config';
 import CircularProgress from "@mui/material/CircularProgress"
+import weatherApi from '../api/weatherApi';
 
 function PlaceSidebar(props: any) {
     const [image, setImage] = useState("");
@@ -11,18 +12,30 @@ function PlaceSidebar(props: any) {
     const getPlaceImage = async () => {
         setImageLoading(true);
         console.log(props.data);
-        const data = await fetch(
-            `https://api.unsplash.com/search/photos?page=1?limit=1&query=${props.data.place_name}&client_id=${config.UNSPLASH_ACCESS}`
-        );
-        const dataJ = await data.json();
-        const result = dataJ.results;
-        console.log(result);
-        setImage(result[0].urls.full);
-        setImageLoading(false);
+        try {
+            const data = await fetch(
+                `https://api.unsplash.com/search/photos?page=1?limit=1&query=${props.data.place_name}&client_id=${config.UNSPLASH_ACCESS}`
+            );
+            const dataJ = await data.json();
+            const result = dataJ.results;
+            console.log(result);
+            if (result && result.length > 0) {
+                setImage(result[0].urls.full);
+            } 
+            setImageLoading(false);
+        } catch (err: any) {
+            console.log(err);
+        }
     };
+
+    const getWeatherData = async () => {
+        const response = await weatherApi(props.data.coordinates[0], props.data.coordinates[1]);
+        console.log(response);
+    }
 
     useEffect(() => {
         getPlaceImage();
+        getWeatherData();
     }, []);
 
 
@@ -43,7 +56,13 @@ function PlaceSidebar(props: any) {
                         :
                         <div>
                             <div style = {{position: "relative", padding: "0px 1rem"}}>
-                                <img src = {image} style = {{width: "100%"}} />
+                                {
+                                    image
+                                    ?
+                                    <img src = {image} style = {{width: "100%"}} alt='No image found' />
+                                    :
+                                    <img src = {require("../images/Image_not_available.png")} style = {{width: "100%"}} alt='No image found' />
+                                }
                             </div>
                         </div>
                     }
