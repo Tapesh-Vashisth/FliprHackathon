b193d4b9c1b9e9d25d3389071f30f821c069348d
saipranithswargam <saipranithswargam@gmail.com>
2023-07-31 13:00:44 +0530

    added itinarary

--- client/src/pages/Itinarary.tsx
+++ client/src/pages/Itinarary.tsx
@@ -1,31 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Itinarary() {
+    const [modal, showModal] = useState(false);
+    const handleCreateItinarary = () => {
+        showModal(true);
+    };
+    const handleDiscardItinarary = () => {
+        showModal(false);
+    };
+    const handleSubmitItinarary = () => {
+      
+    };
     return (
         <div className="itinarary__favourites">
-            <div className="itinarary__favourites--details">
-                <div className="itinarary__favourites--heading">
-                    <h1>Itinararies</h1>
-                </div>
-                <div className="itinarary__favourites--create">
-                    <button>Create Itenarary</button>
-                </div>
-                <div className="itinarary__favourites--itinarary--list">
-                    <div className="itinarary__favourites--itinarary--list--itenarary">
-                        <div className="itinarary__favourites--itinarary--list--itenarary-left">
-                            <div className="itinarary__favourites--itinarary--list--itenarary-left-1">
-                                <p>DateOfThis</p>
+            {!modal && (
+                <div className="itinarary__favourites--details">
+                    <div className="itinarary__favourites--heading">
+                        <h1>Itinararies</h1>
+                    </div>
+                    <div className="itinarary__favourites--create">
+                        <button onClick={handleCreateItinarary}>
+                            Create Itenarary
+                        </button>
+                    </div>
+                    <div className="itinarary__favourites--itinarary--list">
+                        <div className="itinarary__favourites--itinarary--list--itenarary">
+                            <div className="itinarary__favourites--itinarary--list--itenarary-left">
+                                <div className="itinarary__favourites--itinarary--list--itenarary-left-1">
+                                    <p>DateOfThis</p>
+                                </div>
+                                <div className="itinarary__favourites--itinarary--list--itenarary-left-2">
+                                    <p>description of itenerary</p>
+                                </div>
                             </div>
-                            <div className="itinarary__favourites--itinarary--list--itenarary-left-2">
-                                <p>description of itenerary</p>
+                            <div className="itinarary__favourites--itinarary--list--itenarary-right">
+                                <p>List of places to for this day</p>
                             </div>
-                        </div>
-                        <div className="itinarary__favourites--itinarary--list--itenarary-right">
-                            <p>List of places to for this day</p>
                         </div>
                     </div>
                 </div>
-            </div>
+            )}
+            {modal && (
+                <div className="create-itinarary">
+                    <div className="create-itinarary__form">
+                        <form className="create-itinarary__form__mainform">
+                            <h1>Create Itinarary</h1>
+                            <div className="create-itinarary__form__mainform--inputGroup">
+                                <label>Name Of Itinarary</label>
+                                <input />
+                            </div>
+                            <div className="create-itinarary__form__mainform--inputGroup">
+                                <label>Description Of Itinarary</label>
+                                <input />
+                            </div>
+                            <div className="create-itinarary__form__mainform--buttonGroup">
+                                <button>Create</button>
+                                <button onClick={handleDiscardItinarary}>
+                                    Discard
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
