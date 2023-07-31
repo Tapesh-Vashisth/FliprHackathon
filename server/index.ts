4f46406378ed7dc892b1fe0b55ae3968bcc14092
tapesh <tapeshvashisth789@gmail.com>
2023-07-31 15:22:36 +0530

    deploy

--- server/index.ts
+++ server/index.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json({limit: '50mb'}));
 app.use(cors({
-    origin:["http://localhost:3000"],
+    origin:["http://localhost:3000", "https://travelplanner23.netlify.app/"],
     methods:['POST','GET','HEAD','PUT','DELETE'],
     credentials: true
 }))
